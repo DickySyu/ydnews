aa0114aebb948a64b5ae7d483aba2d5d1f8de798
DickySyu <47904116+DickySyu@users.noreply.github.com>
2019-03-13 16:47:11 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = [
+"imei" => "868528027581611",
+"sign" => 
+"028e4a2e4efb9b41f2e34b7d3759940a4fbc0e87",
+"token" => 
+"bad139fbd25e0e805eace6cda9ed0bb7",
+"uuid" => "c3acb5361a114029"
+];
